3acd304d2d856a2bce39590c781817959b2d3a59
jsjphone8 <jsjphone8@gmail.com>
2021-05-02 12:19:39 +0900

    first setting

---
+++ Git-it/ViewController/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Git-it
+//
+//  Created by 정성훈 on 2021/05/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+    }
+
+    private func setUI() {
+        let button: UIButton = { btn in
+            btn.setTitle("임시버튼", for: .normal)
+            btn.setTitleColor(.systemBlue, for: .normal)
+            btn.translatesAutoresizingMaskIntoConstraints = false
+            return btn
+        }(UIButton(type: .system))
+        self.view.addSubview(button)
+        
+        button.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 40).isActive = true
+        button.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 40).isActive = true
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Git-it-ios
 iOS souce code for git-it application
+
+# Requirements  
+1. install pod  
+```bash
+pod install 
+```
+
+2. open `Git-it.xcworkspace`, not `Git-it.xcodeproj`    
