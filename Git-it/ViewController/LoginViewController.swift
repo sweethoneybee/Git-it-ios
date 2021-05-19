c8c95a294c0a0aaf064bc685ac9a1f4ca013ad08
서시언 <siun0816@hanyang.ac.kr>
2021-05-19 22:45:27 +0900

    refactor: 코드 관련 피드백 반영
    1. override mothod를 맨 위로 올림
    2. impicitly unwrapped optional을 없앰
    3. 클로저를 사용해서 라벨, 버튼 등 생성
    4. StoryBoardID 수정 및 어노테이션 objc로 변경
    5. 오타 수정
    
    Resolves:GITIT-46

--- Git-it/ViewController/LoginViewController.swift
+++ Git-it/ViewController/LoginViewController.swift
@@ -8,81 +8,85 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    // MARK: property
+    var labelUserName: UILabel = UILabel()
+    var textFieldUsername: UITextField = UITextField()
+    var buttonSubmit: UIButton = UIButton()
+    var buttonSubmitLater: UIButton = UIButton()
     
-    var textFieldUsername: UITextField!
-    var btnSubmit: UIButton!
-    var btnSubmitLater: UIButton!
-    
-    func addLabelUsername() {
-        let label: UILabel = UILabel()
-        
-        label.frame = CGRect(x: 55, y: 300, width: 200, height: 40)
-        label.font = label.font.withSize(23)
-        label.textColor = UIColor.black
-        label.text = "My username"
-        
-        self.view.addSubview(label)
-    }
-    
-    func addTextFieldUsername() {
-        let textField: UITextField = UITextField()
-        
-        textField.frame = CGRect(x: 60, y: 360, width: 280, height: 40)
-        textField.borderStyle = .roundedRect
-        
-        textFieldUsername = textField
-        self.view.addSubview(textField)
-    }
-    
-    func addBtnSubmit() {
-        let button: UIButton = UIButton()
-        
-        button.frame = CGRect(x: 60, y: 430, width: 280, height: 40)
-        button.setTitle("SUBMIT", for: .normal)
-        button.setTitleColor(UIColor.black, for: .normal)
-        button.backgroundColor = UIColor.gray
-        button.layer.cornerRadius = 8
-        
-        button.addTarget(self, action: #selector(touchUpBtnSubmmit), for: UIControl.Event.touchUpInside)
-        
-        btnSubmit=button
-        self.view.addSubview(button)
-    }
-    
-    func addBtnSubmitLater() {
-        let button: UIButton = UIButton()
-        
-        button.frame = CGRect(x: 100, y: 475, width: 200, height: 40)
-        button.setTitle("I will submit later", for: .normal)
-        button.setTitleColor(UIColor.black, for: .normal)
-        button.backgroundColor = UIColor.white
-        
-        button.addTarget(self, action: #selector(touchUpBtnSubmmitLater), for: UIControl.Event.touchUpInside)
-        
-        btnSubmitLater = button
-        self.view.addSubview(button)
-    }
-    
-    @IBAction func touchUpBtnSubmmit() {
-        UserInfo.username = textFieldUsername.text
-        
-        gotoMain()
-    }
-    
-    @IBAction func touchUpBtnSubmmitLater() {
-        gotoMain()
-    }
-    
-    func gotoMain() {
-        guard let nextVC = self.storyboard?.instantiateViewController(identifier: "main") else {return}; self.present(nextVC, animated: true)
-    }
-    
+    // MARK: override method
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.addLabelUsername()
         self.addTextFieldUsername()
         self.addBtnSubmit()
         self.addBtnSubmitLater()
     }
-
+    
+    // MARK: UI setting mothod
+    func addLabelUsername() {
+        labelUserName = {label in
+            label.frame = CGRect(x: 55, y: 300, width: 200, height: 40)
+            label.font = label.font.withSize(23)
+            label.textColor = UIColor.black
+            label.text = "My username"
+            return label
+        }(UILabel())
+        
+        self.view.addSubview(labelUserName)
+    }
+    
+    func addTextFieldUsername() {
+        textFieldUsername = {textField in
+            textField.frame = CGRect(x: 60, y: 360, width: 280, height: 40)
+            textField.borderStyle = .roundedRect
+            return textField
+        }(UITextField())
+        
+        self.view.addSubview(textFieldUsername)
+    }
+    
+    func addBtnSubmit() {
+        buttonSubmit = {button in
+            button.frame = CGRect(x: 60, y: 430, width: 280, height: 40)
+            button.setTitle("SUBMIT", for: .normal)
+            button.setTitleColor(UIColor.black, for: .normal)
+            button.backgroundColor = UIColor.gray
+            button.layer.cornerRadius = 8
+            button.addTarget(self, action: #selector(touchUpBtnSubmit), for: UIControl.Event.touchUpInside)
+            return button
+        }(UIButton())
+        
+        self.view.addSubview(buttonSubmit)
+    }
+    
+    func addBtnSubmitLater() {
+        buttonSubmitLater = {button in
+            button.frame = CGRect(x: 100, y: 475, width: 200, height: 40)
+            button.setTitle("I will submit later", for: .normal)
+            button.setTitleColor(UIColor.black, for: .normal)
+            button.backgroundColor = UIColor.white
+            button.addTarget(self, action: #selector(touchUpBtnSubmitLater), for: UIControl.Event.touchUpInside)
+            return button
+        }(UIButton())
+        
+        self.view.addSubview(buttonSubmitLater)
+    }
+    
+    // MARK: objc method
+    @objc func touchUpBtnSubmit() {
+        UserInfo.username = textFieldUsername.text
+        gotoMainTabBar()
+    }
+    
+    @objc func touchUpBtnSubmitLater() {
+        gotoMainTabBar()
+    }
+    
+    // MARK: method
+    func gotoMainTabBar() {
+        guard let nextVC = self.storyboard?.instantiateViewController(identifier: "MainTabBar") else {return}
+        self.present(nextVC, animated: true)
+    }
 }
