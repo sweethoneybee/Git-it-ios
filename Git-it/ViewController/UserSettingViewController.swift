ae42859c5b7f6c5a60b07568799f1f7497cb9d12
parkyounbae <55349548+parkyounbae@users.noreply.github.com>
2021-05-20 09:32:00 +0900

    Style: 피드백 관련 수정사항
    클로저의 리턴값을 프로퍼티로 변경
    userNameField에 편집버튼 편입
    버튼 혹은 필드를 누르면 키보드가 올라오게 수정
    키보드에서 리턴을 누르면 키보드가 사라지게 수정
    네비게이션 바 아이템을 이용해 done, cancel버튼 추가
    
    Resolve: GITIT-61

--- Git-it/ViewController/UserSettingViewController.swift
+++ Git-it/ViewController/UserSettingViewController.swift
@@ -22,8 +22,6 @@ class UserSettingViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         
         addUserProfileImage()
         addUserNameField()
