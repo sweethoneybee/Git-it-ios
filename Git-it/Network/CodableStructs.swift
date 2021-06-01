198530ff2b82a3327cb7cd820213c16a55455b76
Jeong <jsjphone8@gmail.com>
2021-06-01 19:50:17 +0900

    feat: 네트워크 클래스 api 구현
    네트워크 요청을 앱 내에서 공통적으로 하기 위해 api 클래스 구현.
    단위 테스트 코드 작성하고 테스트 완료함.
    
    Resolves: GITIT-30

---
+++ Git-it/Network/CodableStructs.swift
@@ -0,0 +1,37 @@
+//
+//  CodableStructs.swift
+//  Git-it
+//
+//  Created by 정성훈 on 2021/05/19.
+//
+
+import Foundation
+
+// MARK: - Decodable
+struct CommitsSummary: Decodable {
+    var validation: String
+    var username: String
+    var commitsRecord: [CommitsRecord]
+    var profileImageUrl: String
+    var commitStreak: Int
+}
+
+struct CommitsRecord: Decodable, Equatable {
+    var count: Int
+    var date: String
+    var level: Int
+}
+
+struct SocialCommitsSummary: Decodable {
+    var validation: String
+    var username: String
+    var commitsRecord: [CommitsRecord]
+}
+
+struct StatsData: Decodable {
+    var validation: String
+    var tier: String
+    var totalCommits: Int
+    var average: Int
+    var maxCommitStreak: Int
+}
---
+++ Git-it/Network/GitItApiProvider.swift
@@ -0,0 +1,161 @@
+//
+//  CommitsApiProvider.swift
+//  Git-it
+//
+//  Created by 정성훈 on 2021/05/18.
+//
+
+import Foundation
+
+enum ApiError: LocalizedError {
+    case noUsernameError
+    case noFriendListError
+    case clientError(Error)
+    case serverError(URLResponse?)
+    case unknownError
+    
+    var errorDescription: String? {
+        switch self {
+        case .noUsernameError: return "no username error"
+        case .noFriendListError: return "no friendList error"
+        case .clientError: return "client error"
+        case .serverError: return "server error"
+        case .unknownError: return "unknown error"
+        }
+    }
+}
+
+enum GitItApi {
+    case commitsSummary(String)
+    case social
+    case stats(String)
+    
+    static let baseUrl = "http://testurl.will.replace/"
+    
+    var path: String {
+        switch self {
+        case .commitsSummary: return "commitsSummary/"
+        case .social: return "social/"
+        case .stats: return "stats/"
+        }
+    }
+    
+    var queryItem: String? {
+        switch self {
+        case .commitsSummary(let username): return "?username=\(username)"
+        case .social: return nil
+        case .stats(let username): return "?username=\(username)"
+        }
+    }
+    
+    var url: URL { URL(string: GitItApi.baseUrl + self.path + (self.queryItem ?? ""))! }
+}
+
+// MARK: - APIs
+/*
+    // asynchronously fetch
+     GitItApiProvider().fetchCommitsSummary { result in
+         switch result {
+         case .failure(let error):
+            // error handling
+         case .success(let commitsSummary):
+            // UI must be handled in Main Queue
+         }
+     }
+ */
+class GitItApiProvider {
+    let session: URLSessionProtocol
+    init(session: URLSessionProtocol = URLSession.shared) {
+        self.session = session
+    }
+    
+    func fetchCommitsSummary(completion: @escaping (Result<CommitsSummary, ApiError>) -> Void) {
+        guard let username = UserInfo.username else {
+            completion(.failure(ApiError.noUsernameError))
+            return
+        }
+        
+        let request = URLRequest(url: GitItApi.commitsSummary(username).url)
+        let task: URLSessionTask = self.session.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(ApiError.clientError(error)))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+               (200...399).contains(httpResponse.statusCode) else {
+                completion(.failure(ApiError.serverError(response)))
+                return
+            }
+            
+            if let data = data, let commitsSummary = try? JSONDecoder().decode(CommitsSummary.self, from: data) {
+                completion(.success(commitsSummary))
+                return
+            }
+            completion(.failure(ApiError.unknownError))
+        }
+        
+        task.resume()
+    }
+    
+    func fetchSocialCommitsSummary(completion: @escaping (Result<[SocialCommitsSummary], ApiError>) -> Void) {
+        guard let friendList = UserInfo.friendList,
+              let httpBody = try? JSONEncoder().encode(friendList) else {
+            completion(.failure(ApiError.noFriendListError))
+            return
+        }
+        
+        var request = URLRequest(url: GitItApi.social.url)
+        request.httpMethod = "POST"
+        request.httpBody = httpBody
+        let task: URLSessionTask = self.session.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(ApiError.clientError(error)))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...399).contains(httpResponse.statusCode) else {
+                completion(.failure(ApiError.serverError(response)))
+                return
+            }
+            
+            if let data = data, let socialCommitsSummaryList = try? JSONDecoder().decode([SocialCommitsSummary].self, from: data) {
+                completion(.success(socialCommitsSummaryList))
+                return
+            }
+            completion(.failure(ApiError.unknownError))
+        }
+        
+        task.resume()
+    }
+    
+    func fetchStatsData(completion: @escaping (Result<StatsData, ApiError>) -> Void) {
+        guard let username = UserInfo.username else {
+            completion(.failure(ApiError.noUsernameError))
+            return
+        }
+        
+        let request = URLRequest(url: GitItApi.stats(username).url)
+        let task: URLSessionTask = self.session.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(ApiError.clientError(error)))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...399).contains(httpResponse.statusCode) else {
+                completion(.failure(ApiError.serverError(response)))
+                return
+            }
+            
+            if let data = data, let statsData = try? JSONDecoder().decode(StatsData.self, from: data) {
+                completion(.success(statsData))
+                return
+            }
+            completion(.failure(ApiError.unknownError))
+        }
+        
+        task.resume()
+    }
+}
