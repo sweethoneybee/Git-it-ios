1e84820355908f86def0357aad094b32f87ed6fa
jsjphone8 <jsjphone8@gmail.com>
2021-06-04 01:12:23 +0900

    feat: ImageCache 클래스 추가, UserInfo의 프로필이미지 페칭 로직 변경
    기존 구현은 UserInfo클래스에서 매번 profileImageData를 가져와서 UIImage를
    만드는 비싼 작업을 수행했었음. 그래서 이것을 캐싱하도록 로직 변경.
    
    UserInfo 클래스 구현이 조금 바뀌어서, 영향을 받는 다른 클래스들 수정 필요함.

---
+++ Git-it/Model/ImageCache.swift
@@ -0,0 +1,52 @@
+//
+//  ImageCache.swift
+//  Git-it
+//
+//  Created by 정성훈 on 2021/06/03.
+//
+
+import UIKit
+import Foundation
+
+class ImageCache {
+    private init() {}
+    
+    static let shared: ImageCache = ImageCache()
+    private let cachedImages = NSCache<NSURL, UIImage>()
+    
+    func image(url: NSURL) -> UIImage? {
+        return cachedImages.object(forKey: url)
+    }
+    
+    func load(url: NSURL, completionHandler: @escaping (UIImage?) -> Void ) {
+        if let image = self.image(url: url) {
+            DispatchQueue.main.async {
+                completionHandler(image)
+            }
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url as URL) { data, response, error in
+            if let _ = error {
+                DispatchQueue.main.async {
+                    completionHandler(nil)
+                    return
+                }
+            }
+            
+            if let httpResponse = response as? HTTPURLResponse,
+               (200...399).contains(httpResponse.statusCode),
+               let imageData = data {
+                DispatchQueue.main.async {
+                    guard let image = UIImage(data: imageData) else {
+                        completionHandler(nil)
+                        return
+                    }
+                    self.cachedImages.setObject(image, forKey: url)
+                    completionHandler(image)
+                    return
+                }
+            }
+        }.resume()
+    }
+}
