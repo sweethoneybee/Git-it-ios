60bff74528cb212bb9ffd15f4b86c11b796e445b
parkyounbae <55349548+parkyounbae@users.noreply.github.com>
2021-09-12 15:57:56 +0900

    feat: HomewView 구현 (#12)
    * feat: HomewView 구현
    
    프로필, 이름, 커밋카운트, 커밋 Streak, 잔디, 새로고침 버튼, 설정 버튼 구현
    정보를 받아오고 만약 정보가 없다면 빈 잔디와 카운트 숫자에는 0 표시, username라벨 탭시 설정 화면으로 넘어감
    화면이 바뀔때 마다 정보가 업데이트 되도록 설정
    
    Resolves: GITIT-47
    
    * refactor: OperationQueue대신에 DispatchQueue 를 사용
    
    간단한 작업에는 더 유리한 DispatchQueue 사용
    
    Resolve: GITIT-47
    
    * test: test code 주석처리
    
    테스트 코드 주석처리
    
    Resolve: GITIT-47
    
    * refact: settingView에서 뒤로가기 버튼을 눌려야 업데이트 하게 수정
    
    원래 뷰가 로드 될때마다 데이터가 업데이트 되었지만 이를 백버튼 눌렀을때만 작동하도록 수정
    정보 업데이트하는 함수를 따로 만듦
    
    Resolve: GITIT-47
    
    * test: 테스트 코드 주석처리
    
    테스트 코드 주석처리
    
    Resolve: GITIT-47
    
    * feat: commit streak 에 days 문구 추가
    
    days 문구 추가
    
    Resolve: GITIT-47
    
    * refactor: 이미지 부분 캐싱 이용
    
    홈 화면 이미지 캐싱 이용
    
    Resolve: GITIT-47
    
    * refactor: 뷰 업데이트 방식 변경
    
    뷰 업데이트 함수를 데이터 패치 이후에 작동하도록 변경
    콜렉션 뷰 플로우레이아웃을 반환값을 통해 할당
    뷰 업데이트시 userinfo에 있던 프로필 이미지 url도 같이 업데이트
    새로고침시 유저 정보를 업데이트
    
    Resolve: GITIT-47
    
    Co-authored-by: sweethoneybee <jsjphone8@gmail.com>

---
+++ Git-it.xcodeproj/LoginViewController.swift
@@ -0,0 +1,140 @@
+//
+//  LoginViewController.swift
+//  Git-it
+//
+//  Created by 서시언 on 2021/05/13.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    // MARK: - property
+    var labelUserName: UILabel?
+    var textFieldUsername: UITextField?
+    var buttonSubmit: UIButton?
+    var buttonSubmitLater: UIButton?
+    
+    // MARK: - override method
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.addLabelUsername()
+        self.addTextFieldUsername()
+        self.addBtnSubmit()
+        self.addBtnSubmitLater()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    // MARK: - UI setting mothod
+    func addLabelUsername() {
+        labelUserName = {label in
+            label.frame = CGRect(x: 55, y: 300, width: 200, height: 40)
+            label.font = label.font.withSize(23)
+            label.textColor = UIColor.black
+            label.text = "My username"
+            return label
+        }(UILabel())
+        
+        if let label = labelUserName {
+            self.view.addSubview(label)
+        }
+    }
+    
+    func addTextFieldUsername() {
+        textFieldUsername = {textField in
+            textField.frame = CGRect(x: 60, y: 360, width: 280, height: 40)
+            textField.borderStyle = .roundedRect
+            textField.delegate = self
+            textField.autocapitalizationType = .none
+            textField.autocorrectionType = .no
+            return textField
+        }(UITextField())
+      
+        if let textField = textFieldUsername {
+            self.view.addSubview(textField)
+        }
+    }
+    
+    func addBtnSubmit() {
+        buttonSubmit = {button in
+            button.frame = CGRect(x: 60, y: 430, width: 280, height: 40)
+            button.setTitle("SUBMIT", for: .normal)
+            button.setTitleColor(UIColor.black, for: .normal)
+            button.layer.cornerRadius = 8
+            button.addTarget(self, action: #selector(touchUpBtnSubmit), for: UIControl.Event.touchUpInside)
+            button.backgroundColor = UIColor.lightGray
+            button.isUserInteractionEnabled = false
+            return button
+        }(UIButton())
+        
+        if let button = buttonSubmit {
+            self.view.addSubview(button)
+        }
+    }
+    
+    func addBtnSubmitLater() {
+        buttonSubmitLater = {button in
+            button.frame = CGRect(x: 100, y: 475, width: 200, height: 40)
+            button.setTitle("I will submit later", for: .normal)
+            button.setTitleColor(UIColor.black, for: .normal)
+            button.backgroundColor = UIColor.white
+            button.addTarget(self, action: #selector(touchUpBtnSubmitLater), for: UIControl.Event.touchUpInside)
+            return button
+        }(UIButton())
+        
+        if let button = buttonSubmitLater {
+            self.view.addSubview(button)
+        }
+    }
+    
+    // MARK: - objc method
+    @objc func touchUpBtnSubmit() {
+        if let textField = textFieldUsername {
+            UserInfo.username = textField.text
+        }
+        gotoMainTabBar()
+    }
+    
+    @objc func touchUpBtnSubmitLater() {
+        gotoMainTabBar()
+    }
+
+    // MARK: - method
+    func gotoMainTabBar() {
+        guard let nextVC = self.storyboard?.instantiateViewController(identifier: "MainTabBar") else {return}
+        nextVC.modalPresentationStyle = .fullScreen
+        self.present(nextVC, animated: true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let text = (textField.text! as NSString).replacingCharacters(in: range, with: string)
+        let utf8Char = string.cString(using: .utf8)
+        let isBackSpace = strcmp(utf8Char, "\\b")
+        
+        if string.hasCharacters() || isBackSpace == -92 {
+            if let button = buttonSubmit {
+                if !text.isEmpty {
+                    button.isUserInteractionEnabled = true
+                    button.backgroundColor = UIColor.gray
+                } else {
+                    button.isUserInteractionEnabled = false
+                    button.backgroundColor = UIColor.lightGray
+                }
+            }
+            return true
+        }
+        return false
+    }
+}
+
+
+
+
